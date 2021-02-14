72031dcd55b6924ceb1621d5b061d6da084516b4
Alexys Hegmann <alexys.hegmann@gmail.com>
2021-02-13 23:09:06 -0600

    Add more experience and education

--- src/features/resume/index.js
+++ src/features/resume/index.js
@@ -13,7 +13,11 @@ import {
   faDatabase,
   faSearch,
   faTerminal,
-  faCube
+  faCube,
+  faChartBar,
+  faFileExcel,
+  faTasks,
+  faGamepad
 } from '@fortawesome/free-solid-svg-icons'
 import {
   faJs,
@@ -22,7 +26,9 @@ import {
   faNodeJs,
   faCss3Alt,
   faSass,
-  faGitAlt
+  faGitAlt,
+  faWordpress,
+  faFacebook
 } from '@fortawesome/free-brands-svg-icons'
 
 const cx = classnames.bind(styles)
@@ -52,7 +58,6 @@ const ResumePage = props => {
               { name: 'CSS+Sass', icon: faSass },
               { name: 'Symfony', icon: faPhp },
               { name: 'Node.js', icon: faNodeJs },
-              { name: 'Koa', icon: faNodeJs },
               { name: 'MySQL', icon: faDatabase },
               { name: 'MongoDB', icon: faDatabase },
               { name: 'ElasticSearch', icon: faSearch },
@@ -67,6 +72,20 @@ const ResumePage = props => {
             dateEnd={moment('2019-04')}
           >
             <p>Frontend and backend development of varied web apps related to market segments in the medical industry and content indexing for the pharma industry.</p>
+            <SkillList compact items={[
+              { name: 'Javascript', icon: faJs },
+              { name: 'React', icon: faReact },
+              { name: 'Redux', icon: faFileCode },
+              { name: 'CSS+Sass', icon: faSass },
+              { name: 'Drupal 7', icon: faPhp },
+              { name: 'Node.js', icon: faNodeJs },
+              { name: 'Koa', icon: faNodeJs },
+              { name: 'MySQL', icon: faDatabase },
+              { name: 'MongoDB', icon: faDatabase },
+              { name: 'ElasticSearch', icon: faSearch },
+              { name: 'Git', icon: faGitAlt },
+              { name: 'Webpack', icon: faCube }
+            ]} />
           </TimelineItem>
           <TimelineItem
             title="Digital Marketing Manager"
@@ -78,10 +97,73 @@ const ResumePage = props => {
             <p>Optimized conversion tools and user experience through web analysis, reducing bouncing rates and increasing lead flow.</p>
             <p>Implemented better practices regarding digital infrastructure security, software development processes and Search Engine Optimization (taking campi pages and landing pages to Google's first page).</p>
             <p>Supervising the optimization and national support of tools like: the CRM, the labour exchange system and others.</p>
+            <SkillList compact items={[
+              { name: 'PHP', icon: faPhp },
+              { name: 'WordPress', icon: faWordpress },
+              { name: 'CSS+Sass', icon: faSass },
+              { name: 'Javascript', icon: faJs },
+              { name: 'MySQL', icon: faDatabase },
+              { name: 'MsSQL Server', icon: faDatabase },
+              { name: 'G Analytics', icon: faChartBar },
+              { name: 'Excel', icon: faFileExcel },
+              { name: 'Project', icon: faTasks }
+            ]} />
+          </TimelineItem>
+          <TimelineItem
+            title="Web Development Coordinator"
+            subtitle="Professional Develoment University"
+            dateStart={moment('2014-02')}
+            dateEnd={moment('2013-09')}
+          >
+            <p>Generated digital strategies based on web statistic analysis, dramatically reducing the overall bouncing rate from 75% to 13%. Developed new lead obtaining tools, increasing Web share in New Enrollment. Developed and maintained an in-house CRM web app, increasing business visibility over the sales funnel.</p>
+            <SkillList compact items={[
+              { name: 'PHP', icon: faPhp },
+              { name: 'WordPress', icon: faWordpress },
+              { name: 'CSS+Sass', icon: faSass },
+              { name: 'Javascript', icon: faJs },
+              { name: 'MySQL', icon: faDatabase },
+              { name: 'MsSQL Server', icon: faDatabase },
+              { name: 'G Analytics', icon: faChartBar },
+              { name: 'Excel', icon: faFileExcel }
+            ]} />
+          </TimelineItem>
+          <TimelineItem
+            title="Web Programmer"
+            subtitle="Professional Develoment University"
+            dateStart={moment('2014-02')}
+            dateEnd={moment('2013-09')}
+          >
+            <p>Developed and maintained a new website focused on lead obtention. Implemented web analytics tools for the first time in the University's marketing department. Developed a complex follow-up system connecting web, mailing and the call center, boosting new enrollment.</p>
+            <SkillList compact items={[
+              { name: 'PHP', icon: faPhp },
+              { name: 'WordPress', icon: faWordpress },
+              { name: 'CSS+Sass', icon: faSass },
+              { name: 'Javascript', icon: faJs },
+              { name: 'MySQL', icon: faDatabase },
+              { name: 'G Analytics', icon: faChartBar }
+            ]} />
+          </TimelineItem>
+          <TimelineItem
+            title="Internship"
+            subtitle="Technological University of Mexico"
+            dateStart={moment('2014-02')}
+            dateEnd={moment('2013-09')}
+          >
+            <p>Built and maintained microsites and landing pages for lead obtention.</p>
+            <p>Developed a Facebook game for the University as a par of a 5 member multidisciplinary team of interns.</p>
+            <SkillList compact items={[
+              { name: 'PHP', icon: faPhp },
+              { name: 'WordPress', icon: faWordpress },
+              { name: 'CSS+Sass', icon: faSass },
+              { name: 'Javascript', icon: faJs },
+              { name: 'MySQL', icon: faDatabase },
+              { name: 'ActionScript 3', icon: faGamepad },
+              { name: 'FB Graph API', icon: faFacebook }
+            ]} />
           </TimelineItem>
         </Timeline>
       </Section>
-      <Section title="Superpowers" icon={faMask} className={cx('half')}>
+      <Section title="Skills" icon={faMask} className={cx('half', 'b-left')}>
         <h2 className={cx('subtitle')}>Frontend</h2>
         <SkillList items={[
           { name: 'Javascript', icon: faJs, description: 'ES6, Vanilla and jQuery' },
@@ -112,6 +194,24 @@ const ResumePage = props => {
           { name: 'Git', icon: faGitAlt, description: 'Git CLI; Github, Bitbucket and Stash' },
           { name: 'Webpack', icon: faCube, description: 'config.js creation' }
         ]} />
+      </Section>
+      <Section title="Education" icon={faBriefcase} className={cx('half')}>
+        <Timeline>
+          <TimelineItem
+            title="Bachelor's Degree, Graphic Design"
+            subtitle="Technological University of Mexico"
+            dateStart={moment('2019-05')}
+            dateEnd="present"
+            icon={<img src={pslLogo} style={{ width: '50px', height: '50px', borderRadius: '50%' }} />}
+          />
+          <TimelineItem
+            title="Technical Course, Graphic Design"
+            subtitle="Montreal Education Center (Mexico)"
+            dateStart={moment('2019-05')}
+            dateEnd="present"
+            icon={<img src={pslLogo} style={{ width: '50px', height: '50px', borderRadius: '50%' }} />}
+          />
+        </Timeline>
       </Section>
     </div>
   )
